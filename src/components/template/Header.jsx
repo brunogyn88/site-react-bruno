a9d696f8274ca60f771aac5bd3e78baa165ef0e6
Bruno Alves <brunogyn.eng@gmail.com>
2020-02-05 17:57:29 -0300

    Bootstrap e Header

--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -1,5 +1,23 @@
-import './Footer.css'
+import './Header.css'
 import React from 'react'
 
 export default props =>
-    <header className="header"></header>
+    <header className="header">
+        <div className="container">
+            <div className="row">
+                <a className="logo col-md-2" href="/"><img src="images/logo.png" alt="Smartek-Life" /></a>
+                <nav className="col-md-8">
+                    <a href="/">Home</a>
+                    <a href="/institucional">Institucional</a>
+                    <a href="/servicos">Serviços</a>
+                    <a href="/incorporacao">INCORPORAÇÃO</a>
+                    <a href="/contato">Contato</a>
+                </nav>
+                <div className="hamburger hamburger--spin col-md-2">
+                    <div className="hamburger-box">
+                        <div className="hamburger-inner"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ src/main/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import React from 'react'
+
+import Main from '../components/template/Main'
+import Footer from '../components/template/Footer'
+
+export default props =>
+    <div className="app">
+        <Main />
+        <Footer />
+    </div>
